0b3aa22e0266710a480b06c2faf3cd48227f1b9e
MiKL <miklgaillard@gmail.com>
2024-01-25 00:33:35 +0100

    Pong

--- projects/miniJeux/pong/Assets/scripts/ball.cs
+++ projects/miniJeux/pong/Assets/scripts/ball.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ball : MonoBehaviour
 {
     public float speed;
     Vector2 dir;
     CamShaker cs;
+    public Text score;
+    public int[] scores;
 
     // la direction par défaut
     void Start()
@@ -24,6 +27,7 @@ public class ball : MonoBehaviour
         if ( (transform.position.y < -3.9f && dir.y < 0)
         ||   (transform.position.y > 5.9f && dir.y > 0) )
         {
+            cs.Shake(0.15f, 0.2f, 20f); // secousse
             dir.y *= -1; // inverse la direction
         }
         // vérifier si la balle sor de l'écran
@@ -31,11 +35,17 @@ public class ball : MonoBehaviour
         {
             // le joueur gange
             transform.position = Vector2.zero;
+            scores[0]++;
+            score.text = scores[1] + " - " + scores[0];
+            cs.Shake(0.55f, 0.6f, 40f);
         }
         if (transform.position.x > 7.5f && dir.x > 0)
         {
             // l'IA gagne
             transform.position = Vector2.zero;
+            scores[1]++;
+            score.text = scores[1] + " - " + scores[0];
+            cs.Shake(0.75f, 0.6f, 40f);
         }
     }
     // gérer les collisions avec les paddles
@@ -46,6 +56,7 @@ public class ball : MonoBehaviour
             bool isPlayer = collision.GetComponent<bar>().isHumanPlayer;
             if ( (isPlayer && dir.x > 0 ) || (isPlayer && dir.x < 0 ) )
             {
+                cs.Shake(0.4f, 0.25f, 30f);
                 dir.x *= -1;
             }
         }
