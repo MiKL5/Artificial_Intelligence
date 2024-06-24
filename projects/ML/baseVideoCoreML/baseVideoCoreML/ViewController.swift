11871c0bfd512965d7b3ec97996cf9879d890629
MiKL5 <miklgaillard@gmail.com>
2024-06-24 16:59:56 +0200

    🎉

---
+++ projects/ML/baseVideoCoreML/baseVideoCoreML/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  baseVideoCoreML
+//
+//  Created by Mickael on 24/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
